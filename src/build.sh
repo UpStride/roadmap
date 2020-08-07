e43d9a8d6f3745c9a47d04367f51c9a827b2c563
Wilder Lopes <wilder.lopes@upstride.io>
2020-08-07 12:35:21 +0200

    Reorganizing files.

---
+++ src/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker build -t upstride/roadmap:1.0 .
---
+++ src/resources.py
@@ -0,0 +1,35 @@
+from flask_restful import Resource
+from flask import render_template
+from flask import make_response
+from flask_httpauth import HTTPBasicAuth
+from utils import constants as constants
+from utils.data import get_spreadsheet, process_dataframe 
+
+
+auth = HTTPBasicAuth()
+
+@auth.verify_password
+def verify(username, password):
+    if not (username and password):
+        return False
+    return constants.USER_DATA.get(username) == password
+
+
+class Home(Resource):
+    def __init__(self, **kwargs):
+        self._variables = {'relpath': '/static/'}
+        self._variables.update(kwargs)
+
+    @auth.login_required
+    def get(self):
+        headers = {'Content-Type': 'text/html'}
+        
+        # Get spreadsheet from Google Drive
+        df = get_spreadsheet(constants.SPREADSHEETNAME)
+        # Generate payload
+        payload = process_dataframe(df)
+
+        self._variables.update({'data': payload})
+
+        return make_response(render_template('index.html', **self._variables), 200, headers)
+
