2be8a3b9ad78215f4423c6a8f27c4c6eaba1c6ad
Wilder Lopes <wilder.lopes@upstride.io>
2020-08-15 20:00:01 +0200

    [docker build] Adding docker.sh file (forgot to add in previous commit).

---
+++ src/docker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Parameters
+IMAGE="upstride/roadmap:1.0"
+PORT="8082"
+SERVICE="roadmap"
+
+# Build container
+docker build -t $IMAGE .
+
+# Run service
+docker run --name $SERVICE -itd --rm \
+           -p $PORT:$PORT \
+            $IMAGE
+
