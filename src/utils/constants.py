5db44984b4281207dbf5e97ebd28fa2c9654ebc7
Wilder Lopes <wilder.lopes@upstride.io>
2020-09-30 10:14:44 +0200

    Updates: cleaning code and adding comments.

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -1,8 +1,16 @@
+# Define simple user authentication 
 USER_DATA = {
     "user": "secret"
 }
+# Define port
 PORT="8082"
+# Spreadsheet file name -- exact name as in Google Docs
 SPREADSHEETNAME = "Roadmap_for_rest_api"
+# Path to Google Docs API key -- instructions to generate it cab be found here: https://developers.google.com/sheets/api/guides/authorizing
 APIKEYPATH = "./roadmap-spreadsheet-d95e6315dd9a.json"
-COLUMNS = ['section', 'task', 'description', 'start_date', 'end_date', 'duration_weeks', 'fte', 'challenges_and_opportunities', 'work_package', 'verrou_technologique', 'team_members', 'private']
+# The spreadsheet columns that should be parsed by the application. Note that
+# you should modify your HTML source (templates/roadmap.html) accordingly in order to display the content of these columns
+COLUMNS = ['section', 'task', 'description', 'start_date', 'end_date', 'duration_weeks', 'fte', 
+           'challenges_and_opportunities', 'project_grant', 'deliverable', 'work_package', 
+           'verrou_technologique', 'team_members', 'private']
 
