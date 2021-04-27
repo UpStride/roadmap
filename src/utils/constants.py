eb6c9d5a2e5e6eccf4fed1a703ff9346d4e3c20e
Wilder Lopes <wilder.lopes@upstride.io>
2021-04-27 19:27:07 +0200

    [thumbnails] Added thumbnails of team members responsible for each task.

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -1,10 +1,11 @@
 # Define simple user authentication 
 USER_DATA = {
-    "user": "secret"
+    "admin": "&H0B0t#p52om19np%7AMBiHN"
 }
 # Define port
 PORT="80"
 # Spreadsheet file name -- exact name as in Google Docs
+# SPREADSHEETNAME = "Roadmap_for_rest_api"
 SPREADSHEETNAME = "Roadmap_for_rest_api"
 # Path to Google Docs API key -- instructions to generate it cab be found here: https://developers.google.com/sheets/api/guides/authorizing
 APIKEYPATH = "./roadmap-spreadsheet-d95e6315dd9a.json"
